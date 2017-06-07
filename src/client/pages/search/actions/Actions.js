ac1701c8be2c876928a4f05b1656a7ae174ed165
dixitsambyal <dxsambyal@gmail.com>
2017-06-07 12:43:53 +0530

    Update Actions.js

--- src/client/pages/search/actions/Actions.js
+++ src/client/pages/search/actions/Actions.js
@@ -1,48 +1,7 @@
-import { history, push } from 'react-router-redux'
-import fetch from 'isomorphic-fetch'
-import {sortBy} from 'lodash'; 
-
-import {
-  REQUEST_COUNTRY_DATA,
-  RECEIVE_COUNTRY_DATA } from './ActionTypes'
-
-const mapCountryData = (countryData,inputText) => {
-    var countryList = [];
-for(var i=0;i<countryData.length;i++)
-    {
-       if(countryData[i].name.toLowerCase().indexOf(inputText.toLowerCase()) !== -1)
-           countryList.push({'country':countryData[i].name,'capital':countryData[i].capital,'continent':countryData[i].region,'population':countryData[i].population,'flag':countryData[i].flag}) 
-    }
-    return countryList;
-}
-
-const requestCountryData = (inputText) => {
-  return {
-    type: REQUEST_COUNTRY_DATA,
-    inputText
-  }
-}
-
-const receiveCountryData = (json,inputText) => {
-  var countryData = [] 
-  json.length ? countryData = sortBy(json, (country) =>{ return country.name; }) : []
-    
-  return {
-    type: RECEIVE_COUNTRY_DATA,
-    countryData : mapCountryData(countryData,inputText)
-  }
-}
-
-export const fetchCountryData = (inputText) => {
-  return (dispatch) => {
-    dispatch(requestCountryData(inputText)
-		
-	const targetUrl = 'https://restcountries.eu/rest/v2/name/'+inputText
-    return fetch(targetUrl)
-      .then((response) => response.json())
-      .then((json) => {
-        dispatch(receiveCountryData(json,inputText))
-      })
-  }
-}
+import { history, push } from 'react-router-redux'import fetch from 'isomorphic-fetch'import {sortBy} from 'lodash'; 
+import {  REQUEST_COUNTRY_DATA,  RECEIVE_COUNTRY_DATA } from './ActionTypes'
+const mapCountryData = (countryData,inputText) => {    var countryList = [];    if(countryData.length >0){   for(var i=0;i<countryData.length;i++)    {       if(countryData[i].name.toLowerCase().indexOf(inputText.toLowerCase()) !== -1)           countryList.push({'country':countryData[i].name,'capital':countryData[i].capital,'continent':countryData[i].region,'population':countryData[i].population,'flag':countryData[i].flag})         }    }    else{        countryList.push({'NO RESULT FOUND':'Give another keyword'})    }    return countryList;}
+const requestCountryData = (inputText) => {  return {    type: REQUEST_COUNTRY_DATA,    inputText  }}
+const receiveCountryData = (json,inputText) => {  var countryData = []  console.log(json);  if(json.status==404)      {          countryData = [];        }    else{        json.length ? countryData = sortBy(json, (country) =>{ return country.name; }) : []    }      return {    type: RECEIVE_COUNTRY_DATA,    countryData : mapCountryData(countryData,inputText)  }}
+export const fetchCountryData = (inputText) => {  return (dispatch) => {    dispatch(requestCountryData(inputText))      let targetUrl='';  if(inputText != ''){  targetUrl = 'https://restcountries.eu/rest/v2/name/'+inputText    }        else{           targetUrl = 'https://restcountries.eu/rest/v2/all'       }console.log(targetUrl);    return fetch(targetUrl)      .then((response) => response.json())      .then((json) => {        dispatch(receiveCountryData(json,inputText))      })  }}
 
