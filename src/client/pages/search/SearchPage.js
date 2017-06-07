6ca7dea1b4cbfaf180ca40e1e91c2659116d924f
dixitsambyal <dxsambyal@gmail.com>
2017-06-07 14:41:07 +0530

    Update SearchPage.js
    Search working on pressing Enter.

--- src/client/pages/search/SearchPage.js
+++ src/client/pages/search/SearchPage.js
@@ -5,23 +5,27 @@ import { fetchCountryData } from './actions/Actions'
 
 class SearchPage extends Component {
 
-  constructor(props) {
-    super(props)
-    this.handleClick = this.handleClick.bind(this);
-      this.handleChange = this.handleChange.bind(this);
-      this.state = {
-      input: '',
-    };
+  constructor(props) {   
+    super(props)   
+    this.fetchData = this.fetchData.bind(this);    
+    this.handleEnter = this.handleEnter.bind(this);      
+    this.handleChange = this.handleChange.bind(this);      
+    this.state = {
+      input: '',   
+    };  
+  }         
+  handleChange(e){
+    this.setState({input: e.target.value});  
   }
-    
-     handleChange(e){
-    this.setState({input: e.target.value});
+  fetchData(){
+    this.props.fetchCountryData(this.state.input); 
+  }    
+  handleEnter(event){ 
+    if(event.keyCode === 13){
+      this.fetchData()     
+    } 
   }
 
-
-  handleClick(){
-    this.props.fetchCountryData(this.state.input);
-  }
 
 
   render() {
@@ -31,7 +35,7 @@ class SearchPage extends Component {
     return (
       <div className="country-search">
         <label className="country-search-label">Search</label>
-        <input className="styles-text-black" type="text" id="one" onChange={this.handleChange}/>
+        <input className="styles-text-black" type="text" id="one" onKeyDown={event => this.handleEnter(event)} onChange={this.handleChange}/>
         <input type ="button" id="button"  value="Submit" onClick={this.handleClick} />
         {countryData != undefined && countryData.length >0 &&
                 <CountryList data={countryData}/>
